7d1ff19c3710eab788e9b1b64edd3ccf88bd86b4
FeQuaresma <felipequada@gmail.com>
2024-10-25 09:38:12 -0300

    Day one

---
+++ ex003/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tipos primitivos em PHP</title>
+</head>
+
+<body>
+  <h1>Teste de tipos primitivos</h1>
+  <?php
+  // 0x = hexa; 0b = binario; 0 = Octal
+  // $num = 0x1A;
+  // echo "O valor da variável é $num";
+
+  $v = 45.2;
+  var_dump($v);
+  ?>
+</body>
+
+</html>
